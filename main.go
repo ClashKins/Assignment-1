1d1d23de1e9798366ed8ab1bd8a2a3d97547ef62
Stefanus Christanto Diantono <113566771+ClashKins@users.noreply.github.com>
2022-11-07 20:35:28 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+type Bio struct {
+	ID        	 int
+	Nama      	 string
+	Alamat    	 string
+	Pekerjaan 	 string
+	Alasan   	 string
+}
+func main() {
+	s := Bio{
+		ID			: 1,
+		Nama		: "Stefanus",
+		Alamat		: "Depok",
+		Pekerjaan	: "Design Interior",
+		Alasan		: "Untuk Mendalami Bahasa Pemrograman Go Lebih Dalam",
+	}
+
+	values := reflect.ValueOf(s)
+	typesOf := values.Type()
+	for i := 0; i < values.NumField(); i++ {
+		fmt.Printf("%s \t : %v \n", typesOf.Field(i).Name, values.Field(i).Interface())
+	}
+
+	var z, sep string
+		for i := 1; i < len(os.Args); i++ {
+			z += sep + os.Args[i]
+			sep = " "
+		}
+}
